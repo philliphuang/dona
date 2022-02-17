e5b099645a6a70dbd25872ce1020a2699dce52ca
Harry Xue <harryxue@macbook-pro.lan>
2022-02-16 22:23:49 -0800

    Added Merchant model

--- server/app/models.py
+++ server/app/models.py
@@ -1,6 +1,9 @@
 from app import db
 from datetime import datetime
 from uuid import uuid1
+from sqlalchemy.dialects.postgresql import JSONB
 
-# class Merchant(db.Model):
-# 	pass
+class Merchant(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	merchant_id = db.Column(db.String(32), index=True, unique=True) # UUID1
+	donation_configs = db.Column(JSONB)
