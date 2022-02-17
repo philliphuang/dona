e9ffa27a56509b88e2845a0c24e142280ab44c39
Harry Xue <harryxue@macbook-pro.lan>
2022-02-16 22:10:21 -0800

    Adding config

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,10 +1,10 @@
 from os import environ
 env = environ.get('FLASK_ENV')
-import logging
 from flask import Flask, Response
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+import config
 
 from flask import request, make_response, redirect, g
 
@@ -12,6 +12,16 @@ from flask import request, make_response, redirect, g
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 app.config['JSON_SORT_KEYS'] = False
+
+if env == 'production':
+	print("Using ProdConfig")
+	app.config.from_object(config.ProdConfig)
+elif env == 'staging':
+	print("Using StagingConfig")
+	app.config.from_object(config.StagingConfig)
+else:
+	print("Using DevConfig")
+	app.config.from_object(config.DevConfig)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
---
+++ server/config.py
@@ -0,0 +1,23 @@
+import os
+from os import environ
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SECRET_KEY = environ['SECRET_KEY']
+	SQLALCHEMY_DATABASE_URI = environ['DATABASE_URI']
+
+class DevConfig(Config):
+	FLASK_ENV = 'development'
+	DEBUG = True
+	TESTING = True
+
+class StagingConfig(Config):
+	FLASK_ENV = 'staging'
+	DEBUG = False
+	TESTING = False
+
+class ProdConfig(Config):
+	FLASK_ENV = 'production'
+	DEBUG = False
+	TESTING = False
