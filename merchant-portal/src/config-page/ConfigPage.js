9881b3081375fd23ea504a1bef8fb5c745a77fdc
Phillip Huang <thephilliphuang@gmail.com>
2022-02-17 17:50:37 -0800

    Styling config page

--- merchant-portal/src/config-page/ConfigPage.js
+++ merchant-portal/src/config-page/ConfigPage.js
@@ -9,33 +9,48 @@ import Box from '@mui/material/Box';
 import DonationComponent from '@demo-organization/demo-scope.ui.donation-component';
 import Button from '@mui/material/Button';
 import Collapse from '@mui/material/Collapse';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 function ConfigCard(props) {
-  const { isActive, config } = props;
+  const { index, isActive, initialConfig, setMerchantConfigs } = props;
+  const [config, setConfig] = React.useState(initialConfig);
+  const [editing, setEditing] = React.useState(false);
   const [selectedOption, setSelectedOption] = React.useState();
-  const [expanded, setExpanded] = React.useState(false);
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+
+  const handleEditClick = () => {
+    setEditing(!editing);
   };
+
   return (
-    <Card variant="outlined">
-      <CardContent>
-        <Typography variant="h6">{config.name}</Typography>
-        <Box sx={{bgcolor:"#e7ebf0", pt:4, pb:4, mt:2,  borderRadius: 2}}>
-          <DonationComponent config={config} setSelectedOption={setSelectedOption}/>
-        </Box>
-      </CardContent>
-      <CardActions>
-        <Button onClick={handleExpandClick}>Edit</Button>
-        <Button>Delete</Button>
-        <Button>Set as active</Button>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+    <div>
+      <Card variant="outlined" sx={isActive ? {borderColor:"success.light", borderWidth:2} : {}}>
         <CardContent>
-          <Typography>Editing options</Typography>
+          <Typography variant="h6" sx={{mb:2}}>{config.name}</Typography>
+          <Box sx={{bgcolor:"#e7ebf0", pt:4, pb:4, mt:2,  borderRadius: 2}}>
+            <DonationComponent config={config} setSelectedOption={setSelectedOption}/>
+          </Box>
         </CardContent>
-      </Collapse>
-    </Card>
+        <CardActions>
+          <Button onClick={handleEditClick} color={editing ? "success" : "primary"}>{editing ? "Save" : "Edit"}</Button>
+          <Button>{isActive ? "Set as inactive" : "Set as active"}</Button>
+          <Box sx={{display: 'flex', flexGrow: 1, justifyContent:"flex-end", flexDirection: "row"}}>
+            <Button color="error">Delete</Button>
+          </Box>
+        </CardActions>
+        <Collapse in={editing} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography>Editing options</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+      {
+        isActive &&
+        <Box sx={{m:1, display: 'flex', flexGrow: 1, justifyContent:"center", flexDirection: "row"}}>
+        <CheckCircleOutlineIcon sx={{color:'success.light', mr:1}}/>
+        <Typography align="center" sx={{fontWeight:"medium"}} color="success.light">ACTIVE</Typography>
+        </Box>
+      }
+    </div>
   );
 }
 
@@ -60,7 +75,6 @@ function ConfigPage() {
     })
     .then(
       (result) => {
-        console.log(result);
         setMerchantConfigs(result);
       },
       (error) => {
@@ -72,14 +86,29 @@ function ConfigPage() {
 
   return (
     <Container maxWidth="sm">
-      <Stack spacing={2} sx={{mt:2, mb:2}}>
+      <Stack spacing={4} sx={{mt:4, mb:4}}>
         {
-          merchantConfigs &&
-            <ConfigCard config={merchantConfigs.active_config} isActive={true}/>
+          merchantConfigs && 
+          merchantConfigs.active_config &&
+            <ConfigCard 
+              initialConfig={merchantConfigs.active_config} 
+              isActive={true}
+              setMerchantConfigs={setMerchantConfigs}
+            />
         }
-        {merchantConfigs && merchantConfigs.inactive_configs.map((config, index) => (
-          <ConfigCard key={index} config={config} isActive={false}/>
-        ))}
+        {
+          merchantConfigs && 
+          merchantConfigs.inactive_configs && 
+          merchantConfigs.inactive_configs.map((config, index) => (
+            <ConfigCard 
+              key={index} 
+              index={index} 
+              initialConfig={config} 
+              isActive={false} 
+              setMerchantConfigs={setMerchantConfigs}
+            />
+          ))
+        }
       </Stack>
     </Container>
   );
