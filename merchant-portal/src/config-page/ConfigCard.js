fe31a6d85b488933c8c1b7103df7bb8e93ea3bc2
Phillip Huang <thephilliphuang@gmail.com>
2022-02-18 21:58:14 -0800

    ActiveToggle and Delete dialogs for Config page

--- merchant-portal/src/config-page/ConfigCard.js
+++ merchant-portal/src/config-page/ConfigCard.js
@@ -7,6 +7,12 @@ import Box from '@mui/material/Box';
 import DonationComponent from '@demo-organization/demo-scope.ui.donation-component';
 import Button from '@mui/material/Button';
 import Collapse from '@mui/material/Collapse';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 function ConfigCard(props) {
@@ -26,8 +32,14 @@ function ConfigCard(props) {
     setEditing(!editing);
   };
 
-  const handleActiveToggleClick = () => {
-    // TODO: modal to confirm this
+  const [activeToggleDialogOpen, setActiveToggleDialogOpen] = React.useState(false);
+  const handleActiveToggleDialogClickOpen = () => {
+    setActiveToggleDialogOpen(true);
+  };
+  const handleActiveToggleDialogClose = () => {
+    setActiveToggleDialogOpen(false);
+  };
+  const handleActiveToggleConfirm = () => {
     if (isActive) {
       setMerchantConfigs(
         (prevMerchantConfigs) => {
@@ -56,10 +68,17 @@ function ConfigCard(props) {
         }
       )
     }
+    handleActiveToggleDialogClose();
   }
 
-  const handleDeleteClick = () => {
-    // TODO: show modal
+  const [deleteDialogOpen, setDeleteDialogOpen] = React.useState(false);
+  const handleDeleteDialogClickOpen = () => {
+    setDeleteDialogOpen(true);
+  };
+  const handleDeleteDialogClose = () => {
+    setDeleteDialogOpen(false);
+  };
+  const handleDeleteConfirm = () => {
     if (isActive) {
       setMerchantConfigs(
         (prevMerchantConfigs) => {
@@ -79,7 +98,12 @@ function ConfigCard(props) {
         }
       )
     }
+    handleDeleteDialogClose();
   }
+
+  
+
+
 
   return (
     <div>
@@ -99,19 +123,67 @@ function ConfigCard(props) {
           </Button>
           <Button 
             disabled={editing} 
-            onClick={handleActiveToggleClick}
+            onClick={handleActiveToggleDialogClickOpen}
           >
             {isActive ? "Set as inactive" : "Set as active"}
           </Button>
+          <Dialog
+            open={activeToggleDialogOpen}
+            onClose={handleActiveToggleDialogClose}
+            maxWidth="xs"
+          >
+            <DialogTitle>
+              {
+                isActive ? 
+                'Set "' + config.name + '" as inactive?' : 
+                'Set "' + config.name + '" as your active configuration?'
+              }
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                {
+                  isActive ? 
+                  "Your checkout systems will stop using this donation configuration immediately." : 
+                  "Your checkout systems will begin using this donation configuration immediately."
+                }
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleActiveToggleDialogClose}>Cancel</Button>
+              <Button onClick={handleActiveToggleConfirm} autoFocus>
+                Set as {isActive ? "Inactive" : "Active"}
+              </Button>
+            </DialogActions>
+          </Dialog>
           <Box sx={{display: 'flex', flexGrow: 1, justifyContent:"flex-end", flexDirection: "row"}}>
             <Button 
               disabled={editing} 
               color="error" 
-              onClick={handleDeleteClick}
+              onClick={handleDeleteDialogClickOpen}
             >
               Delete
             </Button>
           </Box>
+          <Dialog
+            open={deleteDialogOpen}
+            onClose={handleDeleteDialogClose}
+            maxWidth="xs"
+          >
+            <DialogTitle>
+              Delete "{config.name}"?
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                You cannot undo this.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleDeleteDialogClose}>Cancel</Button>
+              <Button onClick={handleDeleteConfirm} autoFocus>
+                Delete
+              </Button>
+            </DialogActions>
+          </Dialog>
         </CardActions>
         <Collapse in={editing} timeout="auto" unmountOnExit>
           <CardContent>
