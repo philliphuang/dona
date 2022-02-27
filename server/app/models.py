17de6a925cf407695a37ea968cdfb935af279338
Harry Xue <harryxue@macbook-pro.lan>
2022-02-26 17:20:24 -0800

    Added Recipient model and updated merchant_info routes to use public_key

--- server/app/models.py
+++ server/app/models.py
@@ -1,9 +1,24 @@
 from app import db
 from datetime import datetime
-from uuid import uuid1
-from sqlalchemy.dialects.postgresql import JSONB
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import JSONB, UUID
 
 class Merchant(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	merchant_id = db.Column(db.String(32), index=True, unique=True) # UUID1
+	public_key = db.Column(db.String(64))
 	donation_configs = db.Column(JSONB)
+
+class Recipient(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	public_key = db.Column(db.String(64))
+	name = db.Column(db.String(300))
+	description = db.Column(db.String(1000))
+
+	def to_dict(self):
+		data = {
+			'public_key': self.public_key,
+			'name': self.name,
+			'description': self.description,
+		}
+
+		return data
--- server/app/routes.py
+++ server/app/routes.py
@@ -1,32 +1,56 @@
 from flask import *
 from app import app
 from app.models import *
-from uuid import uuid1
+from math import ceil
 
 @app.route('/', methods=["GET"])
 def index():
 	return "Welcome!"
 
-@app.route('/api/merchants/<merchant_id>/donation-configs', methods=["GET", "PUT"])
-def user_info(merchant_id):
+@app.route('/api/merchants/<public_key>/donation-configs', methods=["GET", "PUT"])
+def merchant_info(public_key):
 	if request.method == "GET":
-		if merchant_id is None:
-			return {"message": "No merchant_id provided"}, 400
-		merchant = db.session.query(Merchant).filter_by(merchant_id=merchant_id).first()
+		if public_key is None:
+			return {"message": "No public_key provided"}, 400
+		merchant = db.session.query(Merchant).filter_by(public_key=public_key).first()
 
 		if merchant is None:
-			return {"message": "Merchant does not exist for provided merchant_id"}, 404
-		if merchant is not None:
-			return merchant.donation_configs, 200
+			return {"message": "Merchant does not exist for provided public_key"}, 404
+
+		configs_template = merchant.donation_configs
+		request_json = request.get_json()
+
+		for option in configs_template['active_config']['options']:
+			# Update donation amount, purchase amount, and total amount as necessary
+			if request_json:
+				purchase_amount = request_json.get('purchase_amount')
+				if purchase_amount and option['type'] == 'roundup':
+					option['purchase_cents'] = purchase_amount
+					transaction_amount = ceil(purchase_amount/ 100) * 100
+					option['transaction_cents'] = transaction_amount
+					option['donation_cents'] = transaction_amount - purchase_amount
+				elif purchase_amount and option['type'] in set({'fixed', 'input'}):
+					option['purchase_cents'] = purchase_amount
+					option['transaction_cents'] = purchase_amount + option['donation_cents']
+
+			# Load refreshed recipient object
+			recipient = db.session.query(Recipient).filter_by(public_key=option['recipient']['public_key']).first()
+			option['recipient'] = recipient.to_dict()
+
+			# TODO: Populate donation_transfer object
+
+			# TODO: Load donation_transaction request object
+
+		return configs_template, 200
 
 	elif request.method == "PUT":
-		if merchant_id is None:
-			return {"message": "No merchant_id provided"}, 400
+		if public_key is None:
+			return {"message": "No public_key provided"}, 400
 
-		merchant = db.session.query(Merchant).filter_by(merchant_id=merchant_id).first()
+		merchant = db.session.query(Merchant).filter_by(public_key=public_key).first()
 
 		if merchant is None:
-			return {"message": "Merchant does not exist for provided merchant_id"}, 404
+			return {"message": "Merchant does not exist for provided public_key"}, 404
 		if merchant is not None:
 			if request.json.get('donation_configs') is not None:
 				merchant.donation_configs = request.json['donation_configs']
