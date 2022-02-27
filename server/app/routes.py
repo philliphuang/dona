dcb78b57b604cb1acbf44de8cd9ba05f939a1335
Harry Xue <harryxue@macbook-pro.lan>
2022-02-26 18:54:33 -0800

    Set purchase_amount in request params

--- server/app/routes.py
+++ server/app/routes.py
@@ -10,6 +10,8 @@ def index():
 @app.route('/api/merchants/<public_key>/donation-configs', methods=["GET", "PUT"])
 def merchant_info(public_key):
 	if request.method == "GET":
+		print(request.args)
+
 		if public_key is None:
 			return {"message": "No public_key provided"}, 400
 		merchant = db.session.query(Merchant).filter_by(public_key=public_key).first()
@@ -20,28 +22,29 @@ def merchant_info(public_key):
 		configs_template = merchant.donation_configs
 		if not configs_template:
 			return {}, 200
-		request_json = request.get_json()
 
-		for option in configs_template['active_config']['options']:
-			# Update donation amount, purchase amount, and total amount as necessary
-			if request_json:
-				purchase_amount = request_json.get('purchase_amount')
-				if purchase_amount and option['type'] == 'roundup':
+		if 'active_config' in configs_template.keys():
+			for option in configs_template['active_config']['options']:
+				# Update donation amount, purchase amount, and total amount as necessary
+				purchase_amount = request.args.get('purchase_amount')
+				if purchase_amount:
+					purchase_amount = int(purchase_amount)
 					option['purchase_cents'] = purchase_amount
-					transaction_amount = ceil(purchase_amount/ 100) * 100
-					option['transaction_cents'] = transaction_amount
-					option['donation_cents'] = transaction_amount - purchase_amount
-				elif purchase_amount and option['type'] in set({'fixed', 'input'}):
-					option['purchase_cents'] = purchase_amount
-					option['transaction_cents'] = purchase_amount + option['donation_cents']
 
-			# Load refreshed recipient object
-			recipient = db.session.query(Recipient).filter_by(public_key=option['recipient']['public_key']).first()
-			option['recipient'] = recipient.to_dict()
+					if option['type'] == 'roundup':
+						transaction_amount = ceil(purchase_amount/ 100) * 100
+						option['transaction_cents'] = transaction_amount
+						option['donation_cents'] = transaction_amount - purchase_amount
+					elif option['type'] in set({'fixed', 'input'}):
+						option['transaction_cents'] = purchase_amount + option['donation_cents']
 
-			# TODO: Populate donation_transfer object
+				# Load refreshed recipient object
+				recipient = db.session.query(Recipient).filter_by(public_key=option['recipient']['public_key']).first()
+				option['recipient'] = recipient.to_dict()
 
-			# TODO: Load donation_transaction request object
+				# TODO: Populate donation_transfer object
+
+				# TODO: Load donation_transaction request object
 
 		return configs_template, 200
 
