d31415a6bd1d89e8ea722af306e0084638f1cde5
Phillip Huang <thephilliphuang@gmail.com>
2022-02-26 18:22:03 -0800

    Checkout demo framework

---
+++ checkout-demo/src/CheckoutDemo.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+function CheckoutDemo() {
+	return (
+		<Container maxWidth="lg">
+      <Typography variant="h4" align="center">Checkout Donation Demo</Typography>
+    </Container>
+	);
+}
+
+export default CheckoutDemo;
