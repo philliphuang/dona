f20a041d1f7a7bbff14942c33406a28c1a14e382
Phillip Huang <thephilliphuang@gmail.com>
2022-03-02 11:50:57 -0800

    Change input field to UNICEF's material UI version with MUI v5 support

--- donations-component/src/donation-component/InputField.js
+++ donations-component/src/donation-component/InputField.js
@@ -1,41 +1,19 @@
 import * as React from 'react';
-import * as utils from './utils';
-
-import FilledInput from '@mui/material/FilledInput';
-import InputAdornment from '@mui/material/InputAdornment';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-
-// TODO: change to outlined
-// TODO: make all values cents and display as dollars
+import CurrencyTextField from '@demo-organization/demo-scope.ui.currency-text-field';
 
 function InputField(props) {
   const { inputAmount, setInputAmount } = props;
 
-  const handleChange = (event) => {
-    // TODO: replace this with something much more legit
-    setInputAmount(
-      (Number(event.target.value)).toLocaleString(
-        "en-US", 
-        {
-          style:"decimal", 
-          currency:"USD", 
-          maximumFractionDigits:2,
-        }
-      )
-    );
-  };
-
   return (
-    <FormControl variant="filled">
-      <InputLabel htmlFor="filled-adornment-amount">Amount</InputLabel>
-      <FilledInput
-        type="number"
-        onChange={handleChange}
-        value={inputAmount}
-        startAdornment={<InputAdornment position="start">$</InputAdornment>}
-      />
-    </FormControl>
+    <CurrencyTextField
+      sx={{p:4}}
+      label="Amount"
+      variant="standard"
+      value={inputAmount}
+      currencySymbol="$"
+      outputFormat="string"
+      onChange={(event, value)=> setInputAmount(value)}
+    />
   );
 }
 
