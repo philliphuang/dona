1c7f3578143a2b0735010c2b925f1370465f64e1
Phillip Huang <thephilliphuang@gmail.com>
2022-03-15 09:51:42 -0700

    MP: theme baseline

--- merchant-portal/src/utils.js
+++ merchant-portal/src/utils.js
@@ -1,5 +1,5 @@
 export function centsToDollars(cents) {
-  const dollars = (cents / 100).toLocaleString(
+  return (cents / 100).toLocaleString(
     "en-US", 
     {
       style:"decimal", 
@@ -8,10 +8,4 @@ export function centsToDollars(cents) {
       maximumFractionDigits: 2,
     }
   );
-
-  if (cents % 100 === 0) {
-    return dollars.substring(0, dollars.length - 3);
-  } else {
-    return dollars;
-  }
 }
